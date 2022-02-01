e80f7a352fd4e5107e89414df827ed42e36e8ecc
ItzThunderHunts <94800988+ItzDeadThunder@users.noreply.github.com>
2022-02-01 11:45:16 +0600

    Update ban.js

--- commands/⛔️ Moderation Commands/ban.js
+++ commands/⛔️ Moderation Commands/ban.js
@@ -1,5 +1,6 @@
 const Discord = require("discord.js");
 const config = require("../../config.json")
+const emojis = require("../../emojis.json")
 module.exports = {
     name: "ban",
     category: "⛔️ Moderation Commands",
